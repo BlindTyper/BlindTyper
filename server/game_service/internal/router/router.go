ea4720d69c6d0be8605ed6ef9f27ce2928ca8b13
horneioi <solne4no123@gmail.com>
2025-12-01 04:15:16 +0300

    game fixes

--- server/game_service/internal/router/router.go
+++ server/game_service/internal/router/router.go
@@ -2,34 +2,77 @@ package router
 
 import (
 	"context"
+	"game_service/internal/users_provider/user/edit_profile"
 	"log"
 	"net/http"
 	"strings"
 )
 
-var action_routes = map[string]string{
-	"test": "route", // middleware
-}
+// var action_routes = map[string]string{
+// 	"test": "route", // middleware
+// }
 
 func RouteRequest(user, JWT string, ctx context.Context, wrt http.ResponseWriter, req *http.Request) {
 	log.Println("Router got Request. . .")
 
 	path := strings.TrimPrefix(req.URL.Path, "/game/")
+	path_parts := strings.Split(strings.TrimPrefix(path, "/"), "/")
+	log.Println(path_parts[0])
+	switch path_parts[0] {
+	case "online":
+		log.Println("online-request...")
+		/*
+		 chat
+		 lobby
+		 result
+		*/
+	case "user":
+		/*
+			TODO
+			decode JSON and get required method
+		*/
+		UserHander(path_parts[1:], req, wrt)
 
-	switch path {
-	case "/test":
-		log.Printf("path: %s", path)
-		log.Println("test func, got:", user)
+		log.Println("User Handler")
 	default:
 		log.Printf("path: %s", path)
 		http.NotFound(wrt, req)
 	}
 }
 
-/*
-RouteRequest through the middleare
-*/
+func UserHander(parts []string, req *http.Request, wrt http.ResponseWriter, ctx context.Context) {
+	log.Println("User Handler, decompose path.")
 
-/*
-	RouteRequest REST, no access rules.
-*/
+	if len(parts) == 0 {
+		log.Println("User: Empty Route.")
+		return
+	}
+
+	switch parts[0] {
+	case "system":
+		log.Println("System Request -> edit Handler")
+	case "edit":
+		if len(parts) > 1 {
+			command_type := parts[1]
+			/*
+				switch: type edit profile command.
+				also part...
+			*/
+			switch command_type {
+			case "email":
+				edit_profile.ChangeEmail(req, wrt, ctx)
+			case "password":
+			case "nickname":
+			case "image":
+			default:
+				log.Println("Unknown Parameter to edit.")
+			}
+		} else {
+			log.Println("Edit request without id")
+		}
+
+		log.Println("Edit request -> edit Handler")
+	default:
+		log.Println("Unknown Request")
+	}
+}

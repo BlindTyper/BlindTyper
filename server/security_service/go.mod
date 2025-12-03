a419d052b2e3feb71098b4bc1429749037fc9d00
horneioi <bedorev@cs.petrsu.ru>
2025-12-03 04:52:51 +0300

    game_service JWT update

--- server/security_service/go.mod
+++ server/security_service/go.mod
@@ -8,9 +8,17 @@ require (
 )
 
 require (
+	github.com/go-redis/redis/v8 v8.11.5
 	github.com/golang-jwt/jwt/v5 v5.3.0 // direct
+	github.com/google/uuid v1.6.0
 	golang.org/x/net v0.42.0 // indirect
 	golang.org/x/sys v0.34.0 // indirect
 	golang.org/x/text v0.27.0 // indirect
 	google.golang.org/genproto/googleapis/rpc v0.0.0-20250804133106-a7a43d27e69b // indirect
+)
+
+require (
+	github.com/cespare/xxhash/v2 v2.3.0 // indirect
+	github.com/dgryski/go-rendezvous v0.0.0-20200823014737-9f7001d12a5f // indirect
+	github.com/redis/go-redis/v9 v9.17.2
 )
--- server/security_service/internal/JWT/JWTgenerator/JWTgenerate.go
+++ server/security_service/internal/JWT/JWTgenerator/JWTgenerate.go
@@ -1,29 +1,38 @@
 package jwtgenerator
 
 import (
+	"context"
 	"log"
+	redisclient "security_service/internal/redis/client"
 	"time"
 
-	"github.com/golang-jwt/jwt/v5"
+	jwt "github.com/golang-jwt/jwt/v5"
+	"github.com/google/uuid"
 )
 
 /*
 New JWT will be given to the source client on base of his username. ( username + secretword in $env)
 */
 func GenerateJWT(Username string) (answer string, err error) { // return type is JWT. string?
-	// TODO
-	// Move secret Word to $ENV
-	hmacSampleSecret := []byte("SecretWordExample" + Username)
+	/* TODO
+	Move secret Word to $ENV
+	*/
+	ctx := context.Background()
+	hmacSampleSecret := []byte("SecretWordExample")
 
 	/*
 		Setting up the life-time of the given token.
 	*/
+	jti := uuid.NewString()
+	ttl := 168 * time.Hour
+
 	claims := &jwt.RegisteredClaims{
-		ExpiresAt: jwt.NewNumericDate(time.Now().Add(168 * time.Hour)),
+		ExpiresAt: jwt.NewNumericDate(time.Now().Add(ttl)),
 		Issuer:    "Auth-Service",
 		Subject:   Username,
 		IssuedAt:  jwt.NewNumericDate(time.Now()),
 		// JWT ID -> to Redis.
+		ID: jti,
 	}
 
 	token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
@@ -33,5 +42,38 @@ func GenerateJWT(Username string) (answer string, err error) { // return type is
 	}
 
 	log.Println("JWT generated")
+
+	/*
+		TODO -> give token to the redis.
+	*/
+
+	// --- Redis ---
+	// 1. Удаляем старый токен, если есть
+	oldJTI, err := redisclient.Rdb.Get(ctx, "active_token:"+Username).Result()
+	if err == nil && oldJTI != "" {
+		redisclient.Rdb.Del(ctx, "token:"+oldJTI)
+	}
+
+	if oldJTI != "" {
+		// удаляем старый токен, если он есть
+		if err := redisclient.Rdb.Del(ctx, "token:"+oldJTI).Err(); err != nil {
+			return "", err
+		}
+	}
+
+	// 2. Сохраняем новый токен как активный
+	err = redisclient.Rdb.Set(ctx, "token:"+jti, "active", ttl).Err()
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("redis set passed")
+	// 3. Обновляем активный токен пользователя
+	err = redisclient.Rdb.Set(ctx, "active_token:"+Username, jti, ttl).Err()
+	if err != nil {
+		return "", err
+	}
+
+	log.Println("JWT generated and stored in Redis")
 	return signedToken, nil
 }
